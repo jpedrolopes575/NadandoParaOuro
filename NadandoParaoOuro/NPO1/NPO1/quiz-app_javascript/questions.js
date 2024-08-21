c296b8604e47329b453da715fca3770d713155e4
João Pedro Lopes <125674695+jpedrolopes575@users.noreply.github.com>
2024-08-21 11:34:28 -0300

    Add files via upload

---
+++ NadandoParaoOuro/NPO1/NPO1/quiz-app_javascript/questions.js
@@ -0,0 +1,92 @@
+export default [
+  {
+    question: "Qual é o tempo do recorde mundial do César Cielo nos 50m livre?",
+    answers: [
+      { option: "20s91 ", correct: true },
+      { option: "20s87", correct: false },
+      { option: "20s08", correct: false },
+      { option: "20s13", correct: false},
+    ],
+  },
+  {
+    question: "Qual é o maior medalhista olímpico do Brasil?",
+    answers: [
+      { option: "César Cielo", correct: false },
+      { option: "Gustavo Borges", correct: true },
+      { option: "Manoel dos Santos", correct: false },
+      { option: "Bruno Fratus", correct: false},
+    ],
+  },
+  {
+    question: "Em qual estilo Joanna Maranhão ganhou a medalha de bronze no Campeonato Mundial de Esportes Aquáticos?",
+    answers: [
+      { option: "Borboleta", correct: true },
+      { option: "Peito", correct: false },
+      { option: "Livre", correct: false },
+      { option: "Costas", correct: false },
+    ],
+  },
+  {
+    question: "Quantos títulos mundiais Fernando Scherer conquistou em sua carreira?",
+    answers: [
+      { option: "2", correct: false },
+      { option: "4", correct: false },
+      { option: "5", correct: false },
+      { option: "3", correct: true },  
+    ],
+  },
+  {
+  question: "Qual é o melhor tempo pessoal de Bruno Fratus na prova de 50 m livre?",
+    answers: [
+      { option: "20s96", correct: false },
+      { option: "21s", correct: false },
+      { option: "20s98", correct: true },
+      { option: "21s02", correct: false },
+    ],
+  },
+  {
+  question: "Qual foi o tempo que garantiu a Bruno Fratus uma medalha nos 50m livre depois de 12 anos sem um nadador brasileiro conquistar nenhuma?",
+    answers: [
+      { option: "21s40", correct: false },
+      { option: "21.43s", correct: false },
+      { option: "21s38", correct: false },
+      { option: "21s57", correct: true },
+   ],
+ },
+ {
+ question: "Quem foi o primeiro nadador brasileiro a ganhar uma medalha olímpica?",
+   answers: [
+      { option: "Nicholas Santos", correct: false },
+      { option: "Gustavo Borges", correct: false },
+      { option: "Fernando Scherer", correct: false },
+      { option: "Maria Lenk", correct: true },
+    ],
+ },
+ {
+  question: "Em quais Jogos Olímpicos o Brasil ganhou sua primeira medalha na natação?",
+   answers: [
+      { option: "1932 Los Angeles", correct: true },
+      { option: "1948 Londres", correct: false },
+      { option: "1960 Roma", correct: true },
+      { option: "1952 Helsinque", correct: false },
+    ],
+ },
+ {
+  question: "Quantas finais olímpicas a nadadora brasileira Joanna Maranhão chegou em sua carreira?",
+   answers: [
+      { option: "4", correct: true },
+      { option: "7", correct: false },
+      { option: "6", correct: false },
+      { option: "5", correct: true },
+    ],
+ },
+ {
+  question: "Qual foi o tempo que o Guilherme Costas(Cachorrão) garantiu o recorde sul americano nas olimpidas de Paris",
+  answers: [
+     { option: "3:43.56", correct: false },
+     { option: "3:42.76", correct: true },
+     { option: "3:41.65", correct: false },
+     { option: "3:40.49", correct: false },
+   ],
+ },
+];
---
+++ NadandoParaoOuro/NPO1/NPO1/quiz-app_javascript/script.js
@@ -0,0 +1,74 @@
+const question = document.querySelector(".question");
+const answers = document.querySelector(".answers");
+const spnQtd = document.querySelector(".spnQtd");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
+
+import questions from "./questions.js";
+
+let currentIndex = 0;
+let questionsCorrect = 0;
+let shuffledQuestions = [];
+
+// Embaralha as perguntas
+shuffledQuestions = questions.slice().sort(() => Math.random() - 0.5);
+
+// Embaralha as alternativas de cada pergunta
+shuffledQuestions.forEach((question) => {
+  question.answers = question.answers.slice().sort(() => Math.random() - 0.5);
+});
+
+btnRestart.onclick = () => {
+  content.style.display = "flex";
+  contentFinish.style.display = "none";
+
+  currentIndex = 0;
+  questionsCorrect = 0;
+  loadQuestion();
+};
+
+function nextQuestion(e) {
+  if (e.target.getAttribute("data-correct") === "true") {
+    questionsCorrect++;
+  }
+
+  if (currentIndex < shuffledQuestions.length - 1) {
+    currentIndex++;
+    loadQuestion();
+  } else {
+    finish();
+  }
+}
+
+function finish() {
+  textFinish.innerHTML = `você acertou ${questionsCorrect} de ${shuffledQuestions.length}`;
+  content.style.display = "none";
+  contentFinish.style.display = "flex";
+}
+
+function loadQuestion() {
+  spnQtd.innerHTML = `${currentIndex + 1}/${shuffledQuestions.length}`;
+  const item = shuffledQuestions[currentIndex];
+  answers.innerHTML = "";
+  question.innerHTML = item.question;
+
+  item.answers.forEach((answer) => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+    <button class="answer" data-correct="${answer.correct}">
+      ${answer.option}
+    </button>
+    `;
+
+    answers.appendChild(div);
+  });
+
+  document.querySelectorAll(".answer").forEach((item) => {
+    item.addEventListener("click", nextQuestion);
+  });
+}
+
+loadQuestion();
